7f97678372a5176b53ff8a8bc60e56714c9b4aeb
ham-ras <hamrasmuhammad786@gmail.com>
2022-10-20 12:10:30 +0530

    updated

--- gaussian.py
+++ gaussian.py
@@ -1,7 +1,7 @@
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
-
+# dfgd
 X,y=load_iris(return_X_y=True)
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.5,random_state=0)
 gnb=GaussianNB()
